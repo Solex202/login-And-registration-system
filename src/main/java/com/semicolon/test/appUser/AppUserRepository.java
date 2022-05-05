fd65d18eb0a79f7a06e79ee697f14039ae545904
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-05 15:11:31 +0100

    my login and registration system

---
+++ src/main/java/com/semicolon/test/appUser/AppUserRepository.java
@@ -0,0 +1,13 @@
+package com.semicolon.test.appUser;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface AppUserRepository {
+
+    Optional<AppUser> findByEmail(String email);
+}
