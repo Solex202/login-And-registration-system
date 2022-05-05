c1f5dbd8d535ca36fb1b1265a89aa22854df7cf6
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-05 15:23:48 +0100

    my login and registration system

---
+++ src/main/java/com/semicolon/test/registration/RegistrationRequest.java
@@ -0,0 +1,14 @@
+package com.semicolon.test.registration;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+}
