ad81da7c4dc5417d8562e5dc604bbc33f93ef89b
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-05 15:24:05 +0100

    my login and registration system

--- src/main/java/com/semicolon/test/appUser/AppUserService.java
+++ src/main/java/com/semicolon/test/appUser/AppUserService.java
@@ -11,11 +11,13 @@ import org.springframework.stereotype.Service;
 public class AppUserService implements UserDetailsService {
 
     private final AppUserRepository appUserRepository;
-    private  final static String USER_NOT_FOUND = "user with email %s not found";
+    private  final static String USER_NOT_FOUND_MSG = "user with email %s not found";
     @Override
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
-        return appUserRepository.findByEmail(email).orElseThrow(()->
-                new UsernameNotFoundException(String.format(USER_NOT_FOUND, email)));
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(()->
+                new UsernameNotFoundException(
+                        String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
